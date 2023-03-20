043a1230c4055ddc74b48afd87cadc38ab435d06
CrioUser <crio-user@criodo.com>
2023-03-20 10:48:15 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,7 +10,34 @@ import "./Register.css";
 
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
+  const [username,setusrname] = useState("");
+  const [password,setpassword] = useState("");
+  const [confirmpassword,setconfirmpassword]=useState("");
+  const [buffer,setbuffer] = useState(false);
+  
 
+
+  const submit = () => {
+    console.log("submit()");
+    // let usr = e.target[0].value;
+    // let psw = e.target[2].value;
+    let obj = {
+      username: username,
+      password: password,
+    };
+    register(obj);
+  };
+
+  const check=(e)=>{
+    let objdata={
+      username:username,
+      password:password,
+      confirmpassword:confirmpassword,
+
+    };
+    console.log(objdata);
+    validateInput(objdata);
+   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
   /**
@@ -36,6 +63,32 @@ const Register = () => {
    * }
    */
   const register = async (formData) => {
+    setbuffer(true);
+    console.log("register()");
+    axios
+      .post(config.endpoint+"/auth/register", formData)
+      .then((response) => {
+        if (response.status === 201) {
+
+          enqueueSnackbar("Registered successfully",{variant:"success"});
+        }setbuffer(false);
+        
+
+      }
+      )
+      .catch((err) => {
+        console.log(err);
+        if (err.response) {
+          enqueueSnackbar("username is already taken",{variant:"error"});
+          console.log("username already e");
+        } else {
+          enqueueSnackbar(
+            "Something went wrong. Check that the backend is running, reachable and returns valid JSON."
+          ,{variant:"error"});
+        }
+        setbuffer(false);
+      }
+      );
   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
@@ -56,7 +109,32 @@ const Register = () => {
    * -    Check that password field is not less than 6 characters in length - "Password must be at least 6 characters"
    * -    Check that confirmPassword field has the same value as password field - Passwords do not match
    */
+   
   const validateInput = (data) => {
+    if(data.username ===""){
+      enqueueSnackbar("Username is a required field",{variant:"error"});
+      return false;
+    }
+    if((data.username.length) <= 5 ){
+      enqueueSnackbar("Username must be at least 6 characters",{variant:"error"});
+      return false;
+    }
+    if(data.password ===""){
+      enqueueSnackbar("Password is a required field",{variant:"error"});
+      return false;
+    }
+    if(data.password.length <= 5){
+      enqueueSnackbar("Password must be at least 6 characters",{variant:"error"});
+      return false;
+    }
+    if(data.password !== data.confirmpassword){
+      enqueueSnackbar("Passwords do not match",{variant:"error"});
+      return false;
+    }
+    else{   
+    submit();
+  return true;}
+  
   };
 
   return (
@@ -70,41 +148,52 @@ const Register = () => {
       <Box className="content">
         <Stack spacing={2} className="form">
           <h2 className="title">Register</h2>
-          <TextField
-            id="username"
-            label="Username"
-            variant="outlined"
-            title="Username"
-            name="username"
-            placeholder="Enter Username"
-            fullWidth
-          />
-          <TextField
-            id="password"
-            variant="outlined"
-            label="Password"
-            name="password"
-            type="password"
-            helperText="Password must be atleast 6 characters length"
-            fullWidth
-            placeholder="Enter a password with minimum 6 characters"
-          />
-          <TextField
-            id="confirmPassword"
-            variant="outlined"
-            label="Confirm Password"
-            name="confirmPassword"
-            type="password"
-            fullWidth
-          />
-           <Button className="button" variant="contained">
-            Register Now
-           </Button>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log(e);
+              
+              check(e);
+            }}
+          >
+            <TextField
+              id="username"
+              label="Username"
+              variant="outlined"
+              title="Username"
+              name="username"
+              placeholder="Enter Username"
+              onChange={(e) => setusrname(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              variant="outlined"
+              label="Password"
+              name="password"
+              type="password"
+              helperText="Password must be atleast 6 characters length"
+              fullWidth
+              onChange={(e) => setpassword(e.target.value)}
+              placeholder="Enter a password with minimum 6 characters"
+            />
+            <TextField
+              id="confirmPassword"
+              variant="outlined"
+              label="Confirm Password"
+              name="confirmPassword"
+              type="password"
+              fullWidth
+              onChange={(e)=>setconfirmpassword(e.target.value)}
+            />{buffer?<CircularProgress/>:
+            <Button type="submit" className="button" variant="contained" >Register now</Button>
+            }
+          </form>
           <p className="secondary-action">
             Already have an account?{" "}
-             <a className="link" href="#">
+            <a className="link" href="#">
               Login here
-             </a>
+            </a>
           </p>
         </Stack>
       </Box>
